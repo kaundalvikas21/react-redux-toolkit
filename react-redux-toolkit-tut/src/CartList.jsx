d4b9b17b1fd0c4fcc2d6cca182877da658848c25
vikas kaundal <vikaskaundal21@gmail.com>
2025-09-08 17:17:00 +0530

    Cart Item Listing: Display Cart Items, Show total items and price

--- react-redux-toolkit-tut/src/CartList.jsx
+++ react-redux-toolkit-tut/src/CartList.jsx
@@ -1,7 +1,37 @@
+import { useSelector } from "react-redux"
+
 export default function CartList(){
+    const cartSelector = useSelector((state)=>state.cart.items)
+    console.log(cartSelector)
     return(
         <>
-        <h1>cart item Listing</h1>
+        <div className="cart-container">
+            <div className="cart-header">
+                <h2>Cart Items</h2>
+                <span>{cartSelector.length} items</span>
+            </div>
+            {
+                cartSelector.length>0 ? cartSelector.map((item)=>(
+                    <div key={item.id} className="cart-item">
+                        <div className="cart-info">
+                            <img src={item.thumbnail} alt="" />
+                            <div className="item-details">
+                                <h4>{item.title}</h4>
+                                <p>{item.brand}</p>
+                            </div>
+                        </div>
+                        <div className="item-actions">
+                            <span className="price">${item.price}</span>
+                            <button className="btn">Remove</button>
+                        </div>
+                    </div>
+                ))
+                :null
+            }
+            <div className="total-price">
+                Total: ${cartSelector.reduce((sum,item)=>sum+item.price,0)}
+            </div>
+        </div>
         </>
     )
 }
